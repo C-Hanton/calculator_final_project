58db60c70a2391697c596426ed05e7dd7fd56297
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-19 11:47:52 +0100

    add comments for each function

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-
+// addition
 function add (a, b) {
 let result = a + b;
 return result;
 }
-
+// subtraction
 function subtract(a, b){
     let result = a - b;
     return result;
 }
-
+// multiplication
 function multiply(a,b){
     let result = a * b;
     return result;
 }
-
+// division
 function divide(a,b){
     let result = a / b;
     return result;
