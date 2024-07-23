97eb8ca58585492150e2128317d69ab1b5a302e4
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-23 17:48:36 +0100

    initialise variables to link to buttons on index.html

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function (){
+    // store all components of HTML in JS
+    let clear = document.querySelector(".all-clear")
+    let equal = document.querySelector(".equals")
+    let decimal = document.querySelector(".decimal")
+
+    let numbers = document.querySelectorAll(".number")
+    let operators = document.querySelectorAll(".operator")
+
+    let previousScreen
+    let currentScreen
+})
